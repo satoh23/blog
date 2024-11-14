2132414a3b9df6a634e37acfda81cf664940685a
Tetsuya Satoh <satohT@example.com>
2024-11-14 22:14:35 +0900

    ヘッダーをコンポーネントにした

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./Header";
 
 export const metadata: Metadata = {
   title: "よわよわエンジニアの日記",
@@ -13,11 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>
-        <header>Header</header>
-
+      <body className="container mx-auto bg-slate-700 text-slate-50">
+        <Header />
         {children}
-
         <footer>Footer</footer>
       </body>
     </html>
