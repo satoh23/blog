4e9e528cc6edf34f4cc6065571776fbe1be1be64
Tetsuya Satoh <satohT@example.com>
2024-11-15 11:42:29 +0900

    asideの追加

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="py-5 px-10 border-b flex justify-between items-center">
       <div>
         <h1 className="text-2xl font-extrabold">
-          <Link href="/">Next.js13 Blog</Link>
+          <Link href="/">ひよこエンジニアの成長日記</Link>
         </h1>
       </div>
       <div>
