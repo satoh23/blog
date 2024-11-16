66ae1af023ad997390e69a1d3690eff5407af6f9
Tetsuya Satoh <satohT@example.com>
2024-11-16 23:10:48 +0900

    記事一覧画面にcssを当てた

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href="#">
+      <article className="shadow my-4 flex-col">
+        <Link href="#" className="hover:opacity-75">
           <Image
             src="https://picsum.photos/1280"
             alt=""
@@ -15,17 +15,58 @@ const ArticleList = () => {
             style={{ height: "300px" }} // TODO:heightの設定でautoが優先されてしまうためこうしている。それが解決したら削除する。
           />
         </Link>
-        <div>
-          <Link href="#">Technology</Link>
-          <Link href="#">Next.jsの勉強中</Link>
-          <p>By HiyokoEnginner, Published on 2024/11/15</p>
-          <Link href="#">
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold pb-4 hover:text-gray-700"
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2024/11/15</p>
+          <Link href="#" className="text-slate-900 pb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
             commodi animi aliquid facilis optio earum dolorum quisquam?
             Doloribus, non officiis possimus qui aperiam ullam. Debitis corporis
             blanditiis adipisci nihil ex!
           </Link>
-          <Link href="#">続きを読む</Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+      </article>
+      <article className="shadow my-4 flex-col">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://picsum.photos/1280"
+            alt=""
+            width={1280}
+            height={300}
+            style={{ height: "300px" }} // TODO:heightの設定でautoが優先されてしまうためこうしている。それが解決したら削除する。
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold pb-4 hover:text-gray-700"
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2024/11/15</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            commodi animi aliquid facilis optio earum dolorum quisquam?
+            Doloribus, non officiis possimus qui aperiam ullam. Debitis corporis
+            blanditiis adipisci nihil ex!
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
         </div>
       </article>
     </div>
