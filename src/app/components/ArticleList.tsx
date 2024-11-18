6df8e0447b7704d0f0e12685fd683c354e7f2250
Tetsuya Satoh <satoh1529@gmail.com>
2024-11-18 20:30:55 +0900

    APIを叩いて前記事の情報を表示できるようにした

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,74 +1,52 @@
+import { Article } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4 flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://picsum.photos/1280"
-            alt=""
-            width={1280}
-            height={300}
-            style={{ height: "300px" }} // TODO:heightの設定でautoが優先されてしまうためこうしている。それが解決したら削除する。
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
+      {articles.map((article) => (
+        <article className="shadow my-4 flex-col" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
+            <Image
+              src="https://picsum.photos/1280"
+              alt=""
+              width={1280}
+              height={300}
+              style={{ height: "300px" }} // TODO:heightの設定でautoが優先されてしまうためこうしている。それが解決したら削除する。
+            />
           </Link>
-          <Link
-            href="#"
-            className="text-slate-900 text-3xl font-bold pb-4 hover:text-gray-700"
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">Published on 2024/11/15</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-            commodi animi aliquid facilis optio earum dolorum quisquam?
-            Doloribus, non officiis possimus qui aperiam ullam. Debitis corporis
-            blanditiis adipisci nihil ex!
-          </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">
-            続きを読む
-          </Link>
-        </div>
-      </article>
-      <article className="shadow my-4 flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://picsum.photos/1280"
-            alt=""
-            width={1280}
-            height={300}
-            style={{ height: "300px" }} // TODO:heightの設定でautoが優先されてしまうためこうしている。それが解決したら削除する。
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
-          </Link>
-          <Link
-            href="#"
-            className="text-slate-900 text-3xl font-bold pb-4 hover:text-gray-700"
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">Published on 2024/11/15</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-            commodi animi aliquid facilis optio earum dolorum quisquam?
-            Doloribus, non officiis possimus qui aperiam ullam. Debitis corporis
-            blanditiis adipisci nihil ex!
-          </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">
-            続きを読む
-          </Link>
-        </div>
-      </article>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link href="#" className="text-blue-700 pb-4 font-bold">
+              Technology
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 text-3xl font-bold pb-4 hover:text-gray-700"
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pb-3 text-slate-900">{article.createdAt}</p>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 pb-6"
+            >
+              {article.content}
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-pink-800 hover:text-black"
+            >
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
