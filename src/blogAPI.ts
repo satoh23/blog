a63d343ae59f67425b8abf911c47c68e48184c5a
Tetsuya Satoh <satoh1529@gmail.com>
2024-11-19 20:34:55 +0900

    記事新規作成機能を追加

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -27,3 +27,25 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const article = await res.json();
   return article;
 };
+
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+  const currentDatetime = new Date().toISOString();
+  const res = await fetch(`http://localhost:3001/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, title, content, createdAt: currentDatetime }),
+  });
+
+  if (res.ok == false) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  const newArticle = await res.json();
+  return newArticle;
+};
