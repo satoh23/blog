e0e3c7c004091e889e4e9a3a34f2634e9990d580
Tetsuya Satoh <satoh1529@gmail.com>
2024-11-19 11:01:58 +0900

    記事詳細画面を追加

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+
+const Article = ({ params }: { params: { id: string } }) => {
+  return (
+    <div className="max-w-3xl mx-auto p-5">
+      <Image
+        src="https://picsum.photos/1280"
+        alt=""
+        width={1280}
+        height={300}
+        style={{ height: "300px" }} // TODO:heightの設定でautoが優先されてしまうためこうしている。それが解決したら削除する。
+      />
+      <h1 className="text-4xl text-center mb-10 mt-10">タイトル</h1>
+      <div className="text-lg leading-relaxed text-justify">
+        <p>本文</p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
