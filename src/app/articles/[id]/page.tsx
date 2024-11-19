6edb68cc130b49c694b21320607e67fea1023005
Tetsuya Satoh <satoh1529@gmail.com>
2024-11-19 11:22:35 +0900

    APIを使って記事の詳細情報を取得できるようにした

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,7 +1,11 @@
+import { getDetailArticle } from "@/blogAPI";
 import Image from "next/image";
 import React from "react";
 
-const Article = ({ params }: { params: { id: string } }) => {
+const Article = async ({ params }: { params: { id: string } }) => {
+  params = await params;
+  const detailArticle = await getDetailArticle(params.id);
+  console.log(detailArticle);
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,5 +1,5 @@
-import { resolve } from "path";
 import { Article } from "./types";
+import { notFound } from "next/navigation";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch("http://localhost:3001/posts", { cache: "no-store" });
@@ -10,4 +10,20 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 3600 },
+  });
+
+  if (res.status == 404) {
+    notFound();
+  }
+  if (res.ok == false) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  const article = await res.json();
+  return article;
 };
