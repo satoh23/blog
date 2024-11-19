1797af4f0e7b2a678d4fd717c745237169f6893a
Tetsuya Satoh <satoh1529@gmail.com>
2024-11-19 11:30:59 +0900

    APIを使って取得したブログの詳細情報を、ブログ詳細画面に表示されるようにした

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Article = async ({ params }: { params: { id: string } }) => {
   params = await params;
   const detailArticle = await getDetailArticle(params.id);
-  console.log(detailArticle);
+
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
@@ -15,9 +15,11 @@ const Article = async ({ params }: { params: { id: string } }) => {
         height={300}
         style={{ height: "300px" }} // TODO:heightの設定でautoが優先されてしまうためこうしている。それが解決したら削除する。
       />
-      <h1 className="text-4xl text-center mb-10 mt-10">タイトル</h1>
+      <h1 className="text-4xl text-center mb-10 mt-10">
+        {detailArticle.title}
+      </h1>
       <div className="text-lg leading-relaxed text-justify">
-        <p>本文</p>
+        <p>{detailArticle.content}</p>
       </div>
     </div>
   );
