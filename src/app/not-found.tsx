2743d7e60dd09d14d40b8dd74d472bc8dc90361c
Tetsuya Satoh <satoh1529@gmail.com>
2024-11-29 12:45:50 +0900

    404ページを実装。不要なディレクトリを削除

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const NotFound = () => {
   return (
-    <div className="flex items-center justify-center mt-20">
-      <div className="p-8 rounded-lg shadow-md text-center bg-white">
-        <h1 className="text-2xl font-bold mb-4 text-gray-800">404</h1>
-        <p className="text-gray-600">ページが見つかりませんでした。</p>
+    <div className="flex flex-col items-center justify-center min-h-screen pb-60">
+      <Image
+        src="/sappariwakaran-hiyoko.png"
+        alt="困ったひよこ"
+        width={150}
+        height={150}
+        className="mx-auto"
+      />
+      <div className="text-3xl font-extrabold">404</div>
+      <div className="text-3xl font-extrabold">
+        ページが見つかりませんでした
       </div>
+      <Link
+        href={"/"}
+        className="bg-amber-300 p-2 rounded shadow-md border border-amber-400 mt-5"
+      >
+        TOPページへ移動する
+      </Link>
     </div>
   );
 };
