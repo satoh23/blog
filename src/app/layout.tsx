2e4651489bb3ccd0a5401d4d5fbc00dbb15ec878
Tetsuya Satoh <satoh1529@gmail.com>
2024-12-07 13:50:20 +0900

    fix:faviconの設定が一部にしか反映されていないため、設定を変更してみた

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
         <Script src="https://embed.zenn.studio/js/listen-embed-event.js"></Script>
       </head>
       <body
