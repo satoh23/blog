eaf1b9c70e9079ad084d603a5395c8d99e1a0a17
Tetsuya Satoh <satoh1529@gmail.com>
2024-12-11 22:36:21 +0900

    fix:プロフィールのデザインを改善した　

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,12 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Profile = () => {
   return (
     <div className="rounded p-4 mt-4">
       <div className="text-amber-950 font-bold mb-2 pb-2 border-b border-dashed border-amber-950">
         プロフィール
       </div>
-      <p className="mt-2 pl-2">
-        1999年6月生まれ。2023年にIT系の専門学校を卒業してSESに就職。1年半で退職して現在二社目です。エンジニアとして成長するために日々奮闘中。
-      </p>
+      <div className="px-1">
+        <div className="flex">
+          <div className="w-1/3">
+            <div className="flex justify-center">
+              <Image
+                src="/publicdomainq-0047579dercre.png"
+                alt="ひよこSEのアイコン"
+                width={80}
+                height={80}
+                className="rounded-full pt-3"
+              />
+            </div>
+            <div className="flex justify-center mt-5">
+              <Link href="https://github.com/satoh23" target="_blank">
+                <Image
+                  src="/github.svg"
+                  alt="githubのアイコン"
+                  width={30}
+                  height={30}
+                  className="m-1"
+                />
+              </Link>
+              <Link href="https://x.com/hiyoko_frontend" target="_blank">
+                <Image
+                  src="/x.svg"
+                  alt="xのアイコン"
+                  width={30}
+                  height={30}
+                  className="m-1"
+                />
+              </Link>
+            </div>
+          </div>
+          <div className="w-2/3 pl-1">
+            <div className="pt-3 font-extrabold">ひよこSE</div>
+            <div>
+              <p className="opacity-75 mt-1">
+                1999年6月生まれ。2023年にIT系の専門スクールを卒業してSESに就職。その後SIerに転職。エンジニアとして成長するために日々奮闘中！
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
