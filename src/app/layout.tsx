858dfeffebb868a3156d43d9ef1a51946ebc4410
Tetsuya Satoh <satoh1529@gmail.com>
2024-12-13 23:59:59 +0900

    fix:ogpが反映されないため修正した

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import { Suspense } from "react";
 import Loading from "./loading";
 import { zenMaruGothic } from "@/utils/fonts";
 import Script from "next/script";
+import TwitterShareButton from "./components/TwitterShareButton";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "ひよこSEの成長日記",
@@ -29,12 +31,29 @@ export default function RootLayout({
       <head>
         <Script src="https://embed.zenn.studio/js/listen-embed-event.js"></Script>
       </head>
+      <Head>
+        <title>aaaa</title>
+        <meta property="og:title" content="test" />
+        <meta property="og:description" content="testdesu" />
+        <meta
+          property="og:image"
+          content="https://bcmhhfkokrwvzbacftmx.supabase.co/storage/v1/object/public/article/useState.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="test" />
+        <meta name="twitter:description" content="testdesu" />
+        <meta
+          name="twitter:image"
+          content="https://bcmhhfkokrwvzbacftmx.supabase.co/storage/v1/object/public/article/useState.png"
+        />
+      </Head>
       <body
         className={`container mx-auto bg-amber-100 text-amber-900 ${zenMaruGothic.className}`}
       >
         <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex-grow lg:px-14 py-7">
+            <TwitterShareButton />
             <Suspense fallback={<Loading />}>{children}</Suspense>
           </main>
           <Footer />
